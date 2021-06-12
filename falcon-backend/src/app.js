b934ed7af25b89be31127cb66cabd56b73c746c8
ajdin1 <ajdin@getozon.com>
2021-06-12 23:07:07 +0200

    Creating session

--- falcon-backend/src/app.js
+++ falcon-backend/src/app.js
@@ -1,17 +1,41 @@
 const express = require("express");
-const { connectionString } = require("./config/config");
+const session = require("express-session");
+const sessionConfig = require("./config/config");
+const pgSession = require("connect-pg-simple")(session);
 const cors = require("cors");
+const crypto = require("crypto");
 const routes = require("./routes");
+const { connectionString } = require("./config/config");
+const { Pool } = require("pg");
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // enable cors
 app.use(cors());
 app.options("*", cors());
 
-app.use("", routes);
+const sessionDBAccess = new Pool({ connectionString });
+
+app.use(
+  session({
+    store: new pgSession({
+      pool: sessionDBAccess,
+      tableName: "session",
+    }),
+    name: "SID",
+    secret: crypto.randomBytes(15).toString("hex"),
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
+
+app.use(routes);
 
 app.listen(4000, function () {
   console.log("Falcon app listening on port 4000!");
---
+++ falcon-backend/src/lib/passwordUtils.js
@@ -0,0 +1,10 @@
+const crypto = require("crypto");
+
+function validPassword(password, hash, salt) {}
+
+function genPassword(password) {}
+
+module.exports = {
+  validPassword,
+  genPassword,
+};
